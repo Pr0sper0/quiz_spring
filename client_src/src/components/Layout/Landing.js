eaf6ffccdcc7b51199e5926fc490d0d4cb28f800
Pr0sper0 <gestor009@gmail.com>
2020-04-18 10:54:05 +0300

    Register form updated with actions

--- client_src/src/components/Layout/Landing.js
+++ client_src/src/components/Layout/Landing.js
@@ -10,7 +10,7 @@ class Landing extends Component {
             <div className="row">
               <div className="col-md-12 text-center">
                 <h1 className="display-3 mb-4">
-                  Personal Quiz Management Tool
+                  Personal WebDevelopment Quiz Management Tool
                 </h1>
                 <p className="lead">
                   Create your account to join active projects or start your own
